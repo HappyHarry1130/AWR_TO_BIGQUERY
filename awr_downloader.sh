29d6e26d69e1b04168a4827f3d2edb60ef1c8954
HappyHarry1130 <andril.com@outlook.com>
2024-06-01 09:04:04 -0700

    second plan

--- awr_downloader.sh
+++ awr_downloader.sh
@@ -1,37 +1,27 @@
-#
-# This is a template for a downloader "run" script.
-# To use this template:
-# - Change the value of the $DL_DIR_NAME variable to the directory name of your downloader (within '/downloads')
-# - Change the value of the $DL_MAIN_FILE variable to be the path to your downloader's main file ('index.js' or similar in Node.js)
-# - Change the value of the $DL_BQ_DATASET_NAME variable to be the name of the BQ dataset you want to load
-# - Change the value of the $DL_BQ_TABLE_NAME variable to be the name of the BQ table you want to load
+#!/bin/bash
 
+# Variable Initialization
 DL_DIR_NAME=AWR_TO_BIGQUERY
 DL_MAIN_FILE=index.js
 
-
-cd /home/devops/$DL_DIR_NAME
-
+# Ensure reports directory exists
+mkdir -p ./reports
 
 # Get a current date/time stamp
 DATETIME=$(date -Iseconds)
 LOG_FILE_NAME=$(echo $DL_DIR_NAME | sed 's/-/_/g')-$DATETIME.log
 
-
-
-# Get analytics from the downloader and stash them in Cloud Storage.
+# Run Downloader
 node $DL_MAIN_FILE 1> ./reports/$LOG_FILE_NAME 2>&1
 
-
-# END CSV BLOCK
-
-# If any errors occurred, write a message to the console.
-ERROR_COUNT=$(cat ./reports/$LOG_FILE_NAME | grep -ic error)
+# Error Checking
+ERROR_COUNT=$(grep -ic error ./reports/$LOG_FILE_NAME)
 if [ $ERROR_COUNT -ne 0 ]; then
-        echo "One or more errors occurred during $DL_DIR_NAME processing - check the log for details"
+    echo "One or more errors occurred during $DL_DIR_NAME processing - check the log for details"
 fi
 
+# Upload Logs
 gsutil cp ./reports/$LOG_FILE_NAME gs://statbid/$DL_DIR_NAME/$LOG_FILE_NAME
 
-# delete the JSON and log files.
+# Cleanup
 rm ./reports/*
