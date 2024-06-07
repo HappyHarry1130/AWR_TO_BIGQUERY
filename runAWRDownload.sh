8dfa1828dabfbad3df99d908f36752d41ba104af
HappyHarry1130 <andril.com@outlook.com>
2024-06-07 11:23:09 -0700

    real

---
+++ runAWRDownload.sh
@@ -0,0 +1,66 @@
+
+#!/bin/bash
+
+DL_MAIN_FILE=./dist/main.js
+DL_DIR_NAME=awr_downloader
+DL_BQ_DATASET_NAME=AWR
+DL_BQ_TABLE_NAME=Advanced_Web_Ranking
+DL_BQ_TMP_TABLE_NAME=Temp_Advanced_Web_Ranking
+DL_ZIP_DIR_NAME=downloaded
+DL_EXTRACTED_DIR_NAME=extracted
+# gcloud auth activate-service-account --key-file=./service-account-key.json
+
+
+
+node ./dist/main.js
+# PROJECTS=$(jq -r '.projects[]' config.json)
+
+# for PROJECT in $PROJECTS; do
+#     # Run the JavaScript script with Node.js
+#     node ./dist/main.js "$PROJECT"1> ./reports/awr-downloader-$DATETIME.log 2>&1
+#     mkdir -p $DL_EXTRACTED_DIR_NAME
+#     unzip -o $DL_ZIP_DIR_NAME -d $DL_EXTRACTED_DIR_NAME
+#     rm $DL_ZIP_DIR_NAME
+# done
+# DATETIME=$(date -Iseconds)
+
+
+# gsutil cp awr-downloader-combined.csv gs://statbid/$DL_DIR_NAME/awr_downloader_$DATETIME.csv
+
+# find ./reports -type f -exec gsutil cp {} gs://statbid/$DL_DIR_NAME/{}-$DATETIME \; >> ./reports/$LOG_FILE_NAME 2>&1
+
+# ERROR_COUNT=$(cat ./reports/$LOG_FILE_NAME | grep -ic error)
+
+# if [ $ERROR_COUNT -eq 0 ]; then
+#     if ! bq show $DL_BQ_DATASET_NAME &>/dev/null; then
+#         bq mk --dataset $DL_BQ_DATASET_NAME >> ./reports/$LOG_FILE_NAME 2>&1
+#     fi
+
+#     bq load --source_format=CSV --autodetect \
+#     ${DL_BQ_DATASET_NAME}.${DL_BQ_TMP_TABLE_NAME} \
+#     gs://statbid/$DL_DIR_NAME/awr_downloader_$DATETIME.csv >> ./reports/$LOG_FILE_NAME 2>&1
+
+#     if ! bq show --format=prettyjson "${DL_BQ_DATASET_NAME}.${DL_BQ_TABLE_NAME}" > /dev/null 2>&1; then
+#         echo "Table ${DL_BQ_TABLE_NAME} does not exist. Creating table..." >> ./reports/${LOG_FILE_NAME} 2>&1
+#         bq cp -n "${DL_BQ_DATASET_NAME}.${DL_BQ_TMP_TABLE_NAME}" "${DL_BQ_DATASET_NAME}.${DL_BQ_TABLE_NAME}" >> ./reports/${LOG_FILE_NAME} 2>&1
+#     fi
+
+#     bq query --nouse_legacy_sql \
+#     "MERGE \`${DL_BQ_DATASET_NAME}.${DL_BQ_TABLE_NAME}\` T
+#     USING \`${DL_BQ_DATASET_NAME}.${DL_BQ_TMP_TABLE_NAME}\` S
+#     ON CONCAT(T.Date, T.Website, T.Project) = CONCAT(S.Date, S.Website, S.Project)
+#     WHEN NOT MATCHED THEN
+#     INSERT ROW" >> ./reports/${LOG_FILE_NAME} 2>&1
+
+#     bq rm -f -t "${DL_BQ_DATASET_NAME}.${DL_BQ_TMP_TABLE_NAME}" >> ./reports/${LOG_FILE_NAME} 2>&1
+# fi
+
+# ERROR_COUNT=$(grep -ic error ./reports/$LOG_FILE_NAME)
+# if [ $ERROR_COUNT -ne 0 ]; then
+#     echo "One or more errors occurred during $DL_DIR_NAME processing - check the log for details"
+# fi
+
+# gsutil cp ./reports/$LOG_FILE_NAME gs://statbid/$DL_DIR_NAME/$LOG_FILE_NAME
+
+# rm ./reports/*
+# rm awr-downloader-combined.csv
---
+++ src/AWRDownloader.ts
@@ -0,0 +1,149 @@
+/**
+ * This class handles downloading data from AWR.
+ */
+
+import fs from 'fs';
+import axios from 'axios';
+import { DownloaderConfig } from "./dto/DownloaderConfig";
+import { StatbidMessageLogger } from "./helpers/StatbidMessageLogger";
+require('dotenv').config();
+export class AWRDownloader {
+    private static SERVICE_NAME = 'awr-downloader';
+    private logger = new StatbidMessageLogger();
+    private config = new DownloaderConfig();
+    private formattedStartDate = "";
+    private formattedStopDate = "";
+    private credentialsPath = 'service-account-key.json';
+
+    
+    async execute(config: DownloaderConfig) {
+        process.env.GOOGLE_APPLICATION_CREDENTIALS = this.credentialsPath;
+        try {
+            const today = new Date();
+            const startDate = new Date(today);
+            startDate.setDate(today.getDate() - 7);
+
+            this.config = config;
+            this.formattedStartDate = this.formatDate(startDate);
+            this.formattedStopDate = this.formatDate(today);
+
+            const { projects } = config;
+            if (!projects || projects.length === 0) {
+                this.logger.logError(
+                    AWRDownloader.SERVICE_NAME,
+                    'AWR downloader',
+                    "No projects found in config"
+                );
+                return;
+            }
+
+            const processingPromises = projects.map(async (projectName: string) => {
+                if (projectName) {
+                    await this.fetchDataProc(projectName);
+                }
+            });
+            await Promise.all(processingPromises);
+
+            // await this.deleteFolderIfExists('downloaded');
+            // await this.deleteFolderIfExists('extracted');
+        } catch (error: any) {
+            this.logger.logFatal(
+                AWRDownloader.SERVICE_NAME,
+                'AWR downloader',
+                error.message
+            );
+        } finally {
+            this.logger.logInfo(
+                AWRDownloader.SERVICE_NAME,
+                'AWR downloader',
+                `AWR download complete.`
+            )
+        }
+    }
+
+    private async fetchDataProc(projectName: string): Promise<void> {
+        const data = await this.fetchData(projectName);
+        if (data && data.details) {
+            await this.downloadAndExtractZip(data.details, projectName);
+        } else {
+            this.logger.logError(
+                AWRDownloader.SERVICE_NAME,
+                'AWR downloader',
+                "No Data Found"
+            );
+        }
+    }
+
+    private async fetchData(projectName: string): Promise<any | null> {
+        const { apikey: API_KEY } = this.config;
+
+        try {
+            const options = {
+                method: 'GET',
+                url: `https://api.awrcloud.com/v2/get.php?project=${projectName}&startDate=${this.formattedStartDate}&stopDate=${this.formattedStopDate}&addProjectName=yes&encodeurl=false&format=csv&searchEngineId=-1&keywordGroupId=-1&websiteId=-1&addSearchIntent=false&addTitleLinks=false&token=${API_KEY}&action=export_ranking`,
+                headers: { accept: 'application/json' }
+            };
+
+            const response = await axios(options);
+            if (response.data.response_code == 0 || response.data.response_code == 10) {
+                this.logger.logInfo(AWRDownloader.SERVICE_NAME, `AWR_downloader ${projectName}`, 'The data is exist. Downloading')
+                return response.data;
+            }
+            else {
+                this.logger.logWarning(AWRDownloader.SERVICE_NAME, "Something went wrong", 'please check your input again and then try again');
+                return null;
+            }
+        } catch (error: any) {
+            this.logger.logFatal(
+                AWRDownloader.SERVICE_NAME,
+                'AWR downloader',
+                error.message
+            );
+            return null;
+        }
+    }
+
+    private async downloadAndExtractZip(zipFileUrl: string, projectName: string): Promise<void> {
+        const downloadFilePath = `downloaded/${projectName}.zip`;
+
+        const downloadDirectory = downloadFilePath.substring(0, downloadFilePath.lastIndexOf('/'));
+        if (!fs.existsSync(downloadDirectory)) {
+            fs.mkdirSync(downloadDirectory, { recursive: true });
+        }
+
+        try {
+            const response = await axios({
+                method: 'get',
+                url: zipFileUrl,
+                responseType: 'stream'
+            });
+
+            const writer = fs.createWriteStream(downloadFilePath);
+
+            await new Promise((resolve, reject) => {
+                response.data.pipe(writer);
+                writer.on('finish', resolve);
+                writer.on('error', reject);
+            });
+        } catch (error: any) {
+            this.logger.logError(
+                AWRDownloader.SERVICE_NAME,
+                'AWR downloader',
+                `Error downloading or extracting zip: ${error.message}`
+            );
+            throw error;
+        }
+    }
+
+
+
+
+    private formatDate(date: Date): string {
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = date.getFullYear();
+        return `${year}-${month}-${day}`;
+    }
+
+
+}
