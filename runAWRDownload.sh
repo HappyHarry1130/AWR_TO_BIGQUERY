56d7ee09a9a6f8b6aaa504aa63c8b22540adad8b
HappyHarry1130 <andril.com@outlook.com>
2024-06-07 17:13:26 -0700

    update run

--- runAWRDownload.sh
+++ runAWRDownload.sh
@@ -19,8 +19,8 @@ PROJECTS=$(jq -c '.projects' config.json)
 
 echo "$PROJECTS" | jq -r '.[]' | while read -r PROJECT; do
   echo "Project: $PROJECT"
-  node ./dist/main.js "$PROJECT" >> ./reports/$LOG_FILE_NAME-DATETIME.log 2>&1
-  echo "$PROJECT" >> ./reports/$LOG_FILE_NAME-DATETIME.log 2>&1
+  node ./dist/main.js "$PROJECT" >> ./reports/$LOG_FILE_NAME 2>&1
+  echo "$PROJECT" >> ./reports/$LOG_FILE_NAME 2>&1
   mkdir -p $DL_EXTRACTED_DIR_NAME
   unzip -o "$DL_ZIP_DIR_NAME"/"$PROJECT".zip -d "$DL_EXTRACTED_DIR_NAME"
 done
@@ -46,7 +46,7 @@ if [ "$ERROR_COUNT" -ne 0 ]; then
   echo "One or more errors occurred during $DL_DIR_NAME processing - check the log for details"
 fi
 
-gsutil cp ./reports/$LOG_FILE_NAME "$DL_GS_BUCKET_NAME"/reports/${LOG_FILE_NAME}.log
+gsutil cp ./reports/$LOG_FILE_NAME "$DL_GS_BUCKET_NAME"/reports/
 
 # Use -r to remove directories
 rm -r "$DL_ZIP_DIR_NAME"
